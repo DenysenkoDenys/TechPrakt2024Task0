6f4b42a57cc87f4af084d89baeffc302fa0d72e1
Denys Denysenko <ipz224_ddo@student.ztu.edu.ua>
2024-01-29 20:48:10 +0200

    Create task

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+
+            Console.Write("Введіть число 1 :");
+
+            if (!double.TryParse(Console.ReadLine(), out double first))
+            {
+                Console.WriteLine("Введіть коректне число:");
+            }
+            Console.Write("Введіть число 2 :");
+
+            if (!double.TryParse(Console.ReadLine(), out double second))
+            {
+                Console.WriteLine("Введіть коректне число:");
+            }
+            Console.WriteLine($" Cума = {first + second}");
+        }
+    }
+}
